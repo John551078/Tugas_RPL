9d4d8631ac460f5a56a5dcd2466435239181c432
John <programing078@gmail.com>
2025-11-24 20:07:06 +0700

    Pesan commit

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,15 @@
-// eslint.config.js
-import globals from "globals";
+const globals = require("globals");
 
-export default [
-  {
-    languageOptions: {
-      ecmaVersion: 2022,
-      sourceType: "module",
-      globals: {
-        ...globals.node, // Untuk kode Node.js (seperti 'module.exports' atau 'require')
-        ...globals.jest, // Untuk kode Jest di file test
-      }
-    },
-    rules: {
-      // Tambahkan aturan linter dasar di sini
-      "semi": ["error", "always"],
-      "indent": ["error", 2],
-      "no-unused-vars": "warn"
-    }
-  }
-];
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  globals: globals.node,
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {},
+};
---
+++ tests/app.js
@@ -0,0 +1,5 @@
+// tests/app.test.js
+
+test('Contoh Test Sederhana', () => {
+  expect(1 + 1).toBe(2);
+});
